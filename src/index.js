d076853d79eaca8612bf69cb9f5f4ee16ed386df
sidharthbh8 <sidharthbh8@gmail.com>
2023-06-17 14:21:10 +0530

    update

--- src/index.js
+++ src/index.js
@@ -8,9 +8,4 @@ const token = core.getInput('token', { required: true})
 const octokit = new github.getOctokit(token)
 const { owner, repo } = github.context.payload
 console.log(github.context.payload);
-
-const fileData = octokit.rest.pulls.listFiles({
-    owner: owner,
-    repo: repo,
-    pull_number: prNumber,
-  });
+console.log({owner, repo});
