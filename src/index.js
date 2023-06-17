56b0cb88c1080efce018c32bcf721380d9e4ea8f
sidharthbh8 <sidharthbh8@gmail.com>
2023-06-17 14:14:24 +0530

    updated files

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const prNumber = core.getInput('pr_number', { required: true})
 const token = core.getInput('token', { required: true})
 
 const octokit = new github.getOctokit(token)
+const { owner, repo } = github.context.payload
+console.log(github.context.payload);
 
 const fileData = octokit.rest.pulls.listFiles({
     owner,
     repo,
-    pull_number,
+    pull_number: prNumber,
   });
