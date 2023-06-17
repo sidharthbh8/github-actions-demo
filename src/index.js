757cd5921dd2e4625c0c628823770e2fab237a8b
sidharthbh8 <sidharthbh8@gmail.com>
2023-06-17 13:38:48 +0530

    inital files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+const filePath = core.getInput('file_path', { required: true})
+const prNumber = core.getInput('pr_number', { required: true})
+const token = core.getInput('token', { required: true})
+
+const octokit = new github.getOctokit(token)
+
+const fileData = octokit.rest.pulls.listFiles({
+    owner,
+    repo,
+    pull_number,
+  });
