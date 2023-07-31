5affc3aa447d8c1284b0659a3d7c782424ebecbc
sidharthbh8 <sidharthbh8@gmail.com>
2023-08-01 02:24:04 +0530

    callback function valid fix

--- dist/index.js
+++ dist/index.js
@@ -19296,31 +19296,32 @@ const validateCve = async (data) => {
   return;
 }
 
-const cveStructureValidator = async (fileContent, callback) => {
-  if(fileContent === null){
+const cveStructureValidator = (fileContent, callback) => {
+  if (fileContent === null) {
     callback(`Can not read the CVE JSON file`, undefined)
   }
 
-  try {
-    const cveData = JSON.parse(fileContent)
-    const invalid = await validateCve(cveData)
+  const cveData = JSON.parse(fileContent)
+  validateCve(cveData)
+    .then((invalid) => {
 
-    if (invalid) {
-      const errorMessages = invalid.map((error) => {
-        return `At path ${error.instancePath} ${error.message}`;
-      });
-    
-      const errorMessage = errorMessages.join('\n');
-      callback(`Invalid 
-      ${errorMessage}`, undefined)
-    }
-    else {
-      callback(undefined, 'JSON validated, CVE data is in specified structure and contains all the necessary');
-    }
+      if (invalid) {
+        const errorMessages = invalid.map((error) => {
+          return `At path ${error.instancePath} ${error.message}`;
+        });
 
-  } catch (parseError) {
-    callback(parseError, undefined);
-  }  
+        const errorMessage = errorMessages.join('\n');
+        callback(`Invalid 
+        ${errorMessage}`, undefined)
+      }
+      else {
+        callback(undefined, 'JSON validated, CVE data is in specified structure and contains all the necessary');
+      }
+
+    })
+    .catch(parseError => {
+      callback(parseError, undefined)
+    })
 }
 
 cveStructureValidator();
